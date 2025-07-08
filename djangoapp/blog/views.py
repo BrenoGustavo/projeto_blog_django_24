ec44566cb21f2284dedbf48d130d3b7f5ee5ad2a
Brenola <bregutesou@gmail.com>
2025-07-08 01:14:37 -0300

    adding search query

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
 from blog.models import Post
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 PER_PAGE = 9
@@ -21,8 +22,80 @@ def index(request):
         }
     )
 
+def created_by(request, author_pk):
+    posts = Post.objects.get_published()\
+        .filter(created_by__pk=author_pk)
 
-def page(request):
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def tag(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
+
+
+
+def page(request, slug):
+
 
     return render(
         request,
@@ -33,13 +106,18 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = (
+        Post.objects.get_published()
+        .filter(slug=slug)
+        .first()
+    )
 
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
